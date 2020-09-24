a58aa3ab1aa41fe0ac2aeae215aceb1ec14f5e56
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-09-24 23:31:49 +0300

    add webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,17 @@ module.exports = {
         },
       },
       {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+      {
         test: /\.html$/,
         use: [
           {
